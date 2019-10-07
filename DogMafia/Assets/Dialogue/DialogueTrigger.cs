093751adaaa57844aca5e4765b5c0d071590138a
Ana <luckyana7@gmail.com>
2019-10-07 14:30:00 -0500

    dialogue system

---
+++ DogMafia/Assets/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : Interactable
+{
+    public DialogueBase DB;
+    public override void Interact()
+    {
+        Debug.Log("Interacted!");
+        DialogueManager.instance.enqueueDialogue(DB);
+    }
+}
