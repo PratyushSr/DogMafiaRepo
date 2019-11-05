0c8929b163799b8bbe36b77831a2a13eb8c17c03
Pratyush <pratyushshrivastava17@gmail.com>
2019-11-05 17:08:55 -0600

    Rank C duel 1

---
+++ DogMafia/Assets/code/Camera/CameraScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+
+    private Vector3 _StartingPosition;
+    public Transform _FollowTarget;
+    private Vector3 _TargetPos;
+    public float _MoveSpeed;
+
+    void Start()
+    {
+        _StartingPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (_FollowTarget != null)
+        {
+            _TargetPos = new Vector3(_FollowTarget.position.x, _FollowTarget.position.y, transform.position.z);
+            Vector3 velocity = (_TargetPos - transform.position) * _MoveSpeed;
+            transform.position = Vector3.SmoothDamp(transform.position, _TargetPos, ref velocity, 1.0f, Time.deltaTime);
+        }
+    }
+
+    void TrackPlayer()
+    {
+        _FollowTarget = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void TrackTarget(Transform _Target)
+    {
+        _FollowTarget = (_Target ? _FollowTarget : _Target);
+    }
+}
---
+++ DogMafia/Assets/code/Pause Menu/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool _GamePaused = false;
+    public GameObject _PauseMenuUI;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel"))
+        {
+            if(_GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        _PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        _GamePaused = false;
+    }
+
+    void Pause()
+    {
+        _PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        _GamePaused = true;
+    }
+}
